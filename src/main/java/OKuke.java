d3e55e4d56cc0014c981023fb1a252a0f415fa37
OKaiXiang <okaixiang@gmail.com>
2025-08-26 17:52:45 +0800

    Add Echo feature

--- src/main/java/OKuke.java
+++ src/main/java/OKuke.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class OKuke {
     public static void main(String[] args) {
         greetings();
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if(input.equals("bye")) {
+                goodbye();
+                return;
+            }
+            printEcho(input);
+        }
     }
 
     public static void greetings() {
@@ -8,7 +19,18 @@ public class OKuke {
         System.out.println("Hello! I'm OKuke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void goodbye() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
+
+    public static void printEcho(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________");
+    }
+
 }
