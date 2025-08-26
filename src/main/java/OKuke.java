0aa30a3f50184791d1cabedf40dd5887b982522b
OKaiXiang <okaixiang@gmail.com>
2025-08-26 15:35:48 +0800

    Add greetings function

---
+++ src/main/java/OKuke.java
@@ -0,0 +1,14 @@
+public class OKuke {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm OKuke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
