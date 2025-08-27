a2d9beb41ea8fcf17c664059f1a6d4aa73f87312
OKaiXiang <okaixiang@gmail.com>
2025-08-27 16:55:03 +0800

    Add Task class and mark(), unmark()

--- src/main/java/OKuke.java
+++ src/main/java/OKuke.java
@@ -2,38 +2,74 @@ import java.util.Scanner;
 
 public class OKuke {
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         greetings();
-        while(true) {
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            if(input.equals("bye")) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine().trim();
+            String[] parts = line.split(" ");
+            if (line.equals("bye")) {
                 goodbye();
                 return;
             }
-            if(input.equals("list")) {
+            if (line.equals("list")) {
                 listArray(list);
                 continue;
             }
-            addArray(list, input);
-            printEcho(input);
+            if (parts[0].equals("mark")) {
+                int markedNum = Integer.parseInt(parts[1]);
+                mark(list, markedNum);
+                continue;
+            }
+            if (parts[0].equals("unmark")) {
+                int unmarkedNum = Integer.parseInt(parts[1]);
+                unmark(list, unmarkedNum);
+                continue;
+            }
+            addArray(list, line);
+            printEcho(line);
         }
     }
 
-    public static void addArray(String[] input, String message) {
+    public static void mark(Task[] list, int input) {
+        if (input > 100 || input < 1 || list[input - 1] == null) {
+            System.out.println(" Task does not exist");
+            return;
+        }
+        list[input-1].setMark();
+        System.out.println("____________________________________________________________");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(list[input-1].toString());
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void unmark(Task[] list, int input) {
+        if (input > 100 || input < 1 || list[input - 1] == null) {
+            System.out.println(" Task does not exist");
+            return;
+        }
+        list[input-1].unMark();
+        System.out.println("____________________________________________________________");
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(list[input-1].toString());
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void addArray(Task[] input, String message) {
         for (int i = 0; i < input.length; i++) {
             if (input[i] == null) {
-                input[i] = message;
+                input[i] = new Task(message);
                 break;
             }
         }
     }
 
-    public static void listArray(String[] input) {
+    public static void listArray(Task[] input) {
         System.out.println("____________________________________________________________");
+        System.out.println("Here are the tasks in your list:");
         int i = 0;
         while (input[i] != null) {
-            System.out.println(i + 1 + ". " + input[i]);
+            System.out.println(input[i].toString());
             i += 1;
         }
         System.out.println("____________________________________________________________");
---
+++ src/main/java/Task.java
@@ -0,0 +1,42 @@
+public class Task {
+    private String taskName;
+    private boolean isMark;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isMark = false;
+    }
+
+    public Task(String taskName, boolean isMark) {
+        this.taskName = taskName;
+        this.isMark = isMark;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public boolean isMark() {
+        return this.isMark;
+    }
+
+    public void setMark() {
+        this.isMark = true;
+    }
+
+    public void unMark() {
+        this.isMark = false;
+    }
+
+    public String getStatus() {
+        if (this.isMark) {
+            return "X";
+        }
+        return " ";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatus(), this.taskName);
+    }
+}
