28da026cc43a01585a663b2f15e343c9f0d26382
OKaiXiang <okaixiang@gmail.com>
2025-09-09 16:03:54 +0800

    Add unmark assertion

--- src/main/java/okuke/task/TaskList.java
+++ src/main/java/okuke/task/TaskList.java
@@ -90,6 +90,7 @@ public class TaskList {
     public Task unmarkOneBased(int indexOneBased) {
         Task t = tasks.get(indexOneBased - 1);
         t.unMark();
+        assert !t.isDone() : "task should be unmarked"
         return t;
     }
 
